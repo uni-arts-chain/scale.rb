3a0259784a5f1a02c4f6b17a8a886d0f505281d5
xuxh <xxh2611@gmail.com>
2021-07-27 10:26:35 +0800

    update the multiaddress

--- lib/scale/types.rb
+++ lib/scale/types.rb
@@ -368,10 +368,10 @@ module Scale
       def encode
         if value.class == Integer
           index = 1
-          index.to_s(16).rjust(2, "0") + Scale::Types.get("AccountId").new(value).encode
+          index.to_s(16).rjust(2, "0") + Scale::Types.get("Compact").new(value).encode
         elsif value.length == 66 and value[0...2] == '0x'
           index = 0
-          index.to_s(16).rjust(2, "0") + Scale::Types.get("Compact").new(value).encode
+          index.to_s(16).rjust(2, "0") + Scale::Types.get("AccountId").new(value).encode
         elsif value.length == 42
           index = 4
           index.to_s(16).rjust(2, "0") + Scale::Types.get("Address20").new(value).encode
